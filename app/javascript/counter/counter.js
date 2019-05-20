afb4c2d1308b57e2a1c6defa75d7c05dd980604d
Francisco Quintero <frajaquico@yahoo.es>
2019-05-20 13:41:20 -0500

    Add counter module as a webpacker pack

---
+++ app/javascript/counter/counter.js
@@ -0,0 +1,26 @@
+const incrementNode = document.getElementById('increment')
+const decrementNode = document.getElementById('decrement')
+const inputNode = document.getElementById('counter')
+
+const counter = {
+  initialize() {
+    incrementNode.addEventListener('click', (event) => {
+      event.preventDefault()
+
+      const currentValue = inputNode.value
+      inputNode.value = parseInt(currentValue, 0) + 1
+    })
+
+    decrementNode.addEventListener('click', (event) => {
+      event.preventDefault()
+
+      const currentValue = inputNode.value
+
+      if (currentValue > 0) {
+        inputNode.value = parseInt(currentValue, 0) - 1
+      }
+    })
+  }
+}
+
+export default counter
---
+++ app/javascript/counter/index.js
@@ -0,0 +1,5 @@
+import counter from './counter'
+
+document.addEventListener('DOMContentLoaded', () => {
+  counter.initialize()
+})
